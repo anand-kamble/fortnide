968fff68fba2af9d3850f11f4cf01a0df45c9245
anand-kamble <anandmk837@gmail.com>
2022-08-10 23:05:46 +0530

    Added linting rules

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 150,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  singleQuote: true,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as THREE from 'three';
 
 function App() {
-	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-	const renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	const cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-	camera.position.z = 5;
-	function animate() {
-		requestAnimationFrame(animate);
-		renderer.render(scene, camera);
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-	}
-	animate();
+  camera.position.z = 5;
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
+  animate();
 
-	return <span />;
+  return <span />;
 }
 
 export default App;
