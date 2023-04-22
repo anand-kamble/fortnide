f3a2507b1c34231b4cd1635a23b3ab568a055510
anand-kamble <anandmk837@gmail.com>
2023-04-22 22:50:53 +0530

    Projectile class completed

--- src/core/Physics/index.ts
+++ src/core/Physics/index.ts
@@ -1,6 +1,12 @@
 import _physics_world from './physics';
 import * as METHODS from './methods';
+import Projectile from './Projectile';
+import browser_bridge from '../helpers/browser_initiator';
 
 const physics = { ...new _physics_world(), ...METHODS };
+
+browser_bridge.addCallback('mousedown', 'FIREE', () => {
+  console.log(new Projectile().fire().path());
+});
 
 export default physics;
